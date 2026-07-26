efe6c2b58d292cb47cd8287dce07b43714d0df5c
ywnh1 <15900375400@163.com>
2026-07-26 16:51:12 +0000

    chore: remove temp scripts

--- _gen_readme.py
+++
@@ -1,2 +0,0 @@
-import base64,sys
-open("/home/ywnh1/Programs/chain-chess/README.md","wb").write(base64.b64decode(sys.argv[1]))
--- _replace.py
+++
@@ -1,7 +0,0 @@
-import sys
-c=open(sys.argv[1]).read()
-old=sys.argv[2]
-new=sys.argv[3]
-c=c.replace(old,new)
-open(sys.argv[1],"w").write(c)
-print("done")
